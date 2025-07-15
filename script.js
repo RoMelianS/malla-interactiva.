bda7cbed45d006171f2e1d1214a01c1eb2982689
RoMelianS <rociobelen.manriquez@alumnos.ulagos.cl>
2025-07-15 16:52:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+const grid = document.getElementById('grid');
+for (let i = 0; i < 100; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+  grid.appendChild(cell);
+}
